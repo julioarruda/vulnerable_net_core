eeba04f0d19a6898c545c775e6947a4f6a6fcd0f
Julio Arruda <julio.arruda@outlook.com>
2023-11-17 16:08:24 -0800

    [GFTCodeFix]: Update on  vulnerable_asp_net_core/Utils/VulnerableComponent.cs

--- vulnerable_asp_net_core/Utils/VulnerableComponent.cs
+++ vulnerable_asp_net_core/Utils/VulnerableComponent.cs
@@ -2,6 +2,9 @@ namespace vulnerable_asp_net_core.Utils
 {
     public class VulnerableComponent
     {
+        // Incluido por GFT AI Impact Bot
+        protected VulnerableComponent() { }
+
         public static string process(string userInput)
         {
             return "<b>" + userInput + "</b>";
